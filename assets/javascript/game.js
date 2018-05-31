11e060fb8bac6d9b0e8355e70b3ff1e4866ef558
Kamon <Kamon.kirker@gmail.com>
2018-05-30 20:00:58 -0700

    last problem is to get keypress to replace dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,6 @@
 
         let winsNumber = 0
         let guessesNumber = 13;
-        
         let randomWord = [
         'lava',
         'mortal kombat',
@@ -19,23 +18,63 @@
         'javascript',
         'jquery',
         'homework'];
-        //console.log(randomWord[0]);
+
+        //just checking array exists fine in colse-console.log(randomWord[0]);
 
         $('#wins-display').html('Wins : ' + winsNumber);
-
+        
+        //chooses random word from array options above
         let word = randomWord[Math.floor(Math.random() * randomWord.length)];
-
+        //consoling to verify word chosen correctly, or if you want to open console and cheat
         console.log(word);
 
+
+
+
+        
+        //iterating through the randomly selected word and changing string character to an underscore. 
         let emptyAnswer = [];
-        for (i = 0 ; i < word.length ; i++) {
+        for (let i = 0 ; i < word.length ; i++) {
             emptyAnswer[i] = '_';
         }
 
         let remaingLetters = word.length;
 
-        console.log(emptyAnswer);
+        //console.log(emptyAnswer);
+
+
+        let letterGuessed = [];
         
+
+    document.onkeydown = function (e) {
+            let keyPress;
+
+
+        if (typeof event !== 'undefined') {
+            keyPress = event.keyCode;
+        }
+        else if (e) {
+                keyPress = e.which;
+        }
+
+        letterGuessed.push(String.fromCharCode(keyPress));
+
+        console.log(letterGuessed);
+
+
+
+        let newHTML = [];
+        for (let index = 0; index < letterGuessed.length; index++) {
+            let lettersGuessed = letterGuessed[index];
+            newHTML.push(lettersGuessed);
+
+        };
+        $('#letters-guessed').html(newHTML.join(''));
+
+
+        return false;   // Prevents the default action
+    };
+       
 
         $('body').keydown(function() {
             //console.log('a key was pressed');
@@ -43,17 +82,12 @@
             $('#dashes').html(emptyAnswer);
             
             guessesNumber--;
-            //console.log(guessesNumber);
+            console.log(guessesNumber);
             $('#guesses-remaining').html('Guesses Remaining: ' + guessesNumber);
-
 
             if (guessesNumber < 1) {
                 $('#guesses-remaining').html("You have lost, may God have mercy upon your soul");
             };
-
-
-            
-
 
             if (emptyAnswer === word) {
                 winsNumber++;
